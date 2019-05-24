56aba08ab7dcdbde6bc56524574f2d5b42a31f71
OldJimmy <thomas.loercher42@gmail.com>
2019-05-24 17:49:01 +0200

    Initial commit

---
+++ src/main/java/com/oldjimmy/iximam/IxiMamApplication.java
@@ -0,0 +1,13 @@
+package com.oldjimmy.iximam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IxiMamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IxiMamApplication.class, args);
+	}
+
+}
